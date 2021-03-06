40f337101495d1d6c224d6d0e3a8d3ad08775fdc
wrack <carlosveiga1618@gmail.com>
2021-03-06 18:26:15 -0300

    Implement App component that displays the  BooksList and BooksForm components. Render App in the main index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const App = () => (
-  <>
-    <h1>Hello World!</h1>
-  </>
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />,
+  document.getElementById('root'));
