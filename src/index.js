451689d5313493d1f6ba4a860772d3209fd43939
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-07 00:09:45 +0100

    create reducer and default state for the store.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import booksReducer from './reducers/books';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+const defaultBooks = [
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'The war through the history',
+    category: 'History',
+
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Harry Potter',
+    category: 'Action',
+
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'life of Bill Gates',
+    category: 'Biography',
+
+  },
+];
+
+const store = createStore(booksReducer(defaultBooks));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+
+  </Provider>,
+  document.getElementById('root'),
+);
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,6 +1,6 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
-const booksReducer = (state = [], action) => {
+const booksReducer = (state, action) => {
   switch (action.type) {
     case CREATE_BOOK:
       return [...state, action.payload];
