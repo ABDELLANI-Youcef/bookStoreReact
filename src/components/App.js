215b373c32501a6f7f737a31ef153e387563ee14
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-07 12:34:04 +0100

    move BooksForm, BooksLint and Books classes to containers folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import BooksList from './BooksList';
-import BooksForm from './BooksForm';
+import BooksList from '../containers/BooksList';
+import BooksForm from '../containers/BooksForm';
 
 const App = () => (
   <div>
