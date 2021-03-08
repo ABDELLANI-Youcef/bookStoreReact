b905d0637238ce56c78e7df6ba639b2ebdcb2550
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-08 23:31:59 +0100

    filter the categories according to the filter.

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,31 +2,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from './Book';
+import CategoryFilter from './CategoryFilter';
+import { changeFilter } from '../actions/index';
 
-const BooksList = ({ books }) => (
-
-  <table>
-    <thead>
-      <tr>
-        <th>Book ID</th>
-        <th>Title</th>
-        <th>Category</th>
-      </tr>
-    </thead>
-    <tbody>
-      {books.map(book => (
-        <Book book={book} key={book.id} />
-      ))}
-    </tbody>
-  </table>
-);
-
+const BooksList = ({ books, changeFilter, filter }) => {
+  const handleFilterChange = filter => {
+    if (filter === 'All') {
+      changeFilter('');
+    } else {
+      changeFilter(filter);
+    }
+  };
+  return (
+    <>
+      <CategoryFilter clickHandle={handleFilterChange} />
+      <table>
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.filter(book => book.category === filter || filter === '').map(book => (
+            <Book book={book} key={book.id} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object).isRequired,
+  filter: PropTypes.string.isRequired,
+  changeFilter: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   books: state.books,
+  filter: state.filter,
 });
 
-export default connect(mapStateToProps)(BooksList);
+const mapDispatch = {
+  changeFilter,
+};
+
+export default connect(mapStateToProps, mapDispatch)(BooksList);
