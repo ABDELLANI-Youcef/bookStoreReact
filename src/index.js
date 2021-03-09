13a6764de6aeb2bd9cb08365b243dd0e1b3056bf
Youcef ABDELLANI <youcefabdellani@gmail.com>
2021-03-10 00:17:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './styles/index.css';
 
 const rootReducer = combineReducers({ books: booksReducer, filter: filterReducer });
 const store = createStore(rootReducer);
+
 ReactDOM.render(
   <ChakraProvider>
     <Provider store={store}>
