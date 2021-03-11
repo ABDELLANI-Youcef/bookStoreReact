3051c06ed35b1d50a21544717403c276b3742cf1
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-11 19:18:56 +0100

    fetch data from server and load it in redux.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,58 +1,27 @@
-const booksAlready = async () => {
+import { createBook } from '../actions/index';
+
+export const booksAlready = async store => {
+  let booksData;
   try {
     const options = {
       method: 'GET',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        mode: 'no-cors',
-        'Access-Control-Allow-Origin': '*',
+        mode: 'cors',
       },
     };
     const response = await fetch('https://tranquil-caverns-54399.herokuapp.com/books', options);
-    const booksData = await response.json();
-    console.log(booksData);
-    // const state = weatherData.weather[0].main;
-    // const tempDegree = (weatherData.main.temp - 273.15).toFixed(1);
-    // getPicture(state);
-    // resultsShow.style.display = 'block';
-    // cityName.textContent = place;
-    // weather.textContent = state;
-
-    // temperatureC.textContent = `${tempDegree}°C`;
-    // temperatureF.textContent = `${degCelToFeh(tempDegree)}°F`;
-    // humidity.textContent = weatherData.main.humidity;
-    // windSpeed.textContent = `${(weatherData.wind.speed * 3.6).toFixed(2)}`;
-    // const d = (((weatherData.wind.deg + 360 - 22.5) / 45) % 8).toFixed(0);
-    // windDirection.textContent = directions[d];
-    // errMessage.style.display = 'none';
+    booksData = await response.json();
+    booksData.forEach(book => {
+      store.dispatch(createBook(book));
+    });
   } catch (error) {
-    // errMessage.style.display = 'block';
-    console.error(error);
+    return error;
   }
+  return booksData;
 };
 
-booksAlready();
-
-const defaultBooks = [
-  {
-    id: Math.floor(Math.random() * 1000),
-    title: 'The war through the history',
-    category: 'History',
-
-  },
-  {
-    id: Math.floor(Math.random() * 1000),
-    title: 'Harry Potter',
-    category: 'Action',
-
-  },
-  {
-    id: Math.floor(Math.random() * 1000),
-    title: 'Life of Bill Gates',
-    category: 'Biography',
-
-  },
-];
+const defaultBooks = [];
 
 export default defaultBooks;
