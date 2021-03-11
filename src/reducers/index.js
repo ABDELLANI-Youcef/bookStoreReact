9790b3f1a843b8d334c67c65d409cb0664e8d219
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-11 18:35:56 +0100

    add fetch method to books index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,39 @@
+const booksAlready = async () => {
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        mode: 'no-cors',
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+    const response = await fetch('https://tranquil-caverns-54399.herokuapp.com/books', options);
+    const booksData = await response.json();
+    console.log(booksData);
+    // const state = weatherData.weather[0].main;
+    // const tempDegree = (weatherData.main.temp - 273.15).toFixed(1);
+    // getPicture(state);
+    // resultsShow.style.display = 'block';
+    // cityName.textContent = place;
+    // weather.textContent = state;
+
+    // temperatureC.textContent = `${tempDegree}°C`;
+    // temperatureF.textContent = `${degCelToFeh(tempDegree)}°F`;
+    // humidity.textContent = weatherData.main.humidity;
+    // windSpeed.textContent = `${(weatherData.wind.speed * 3.6).toFixed(2)}`;
+    // const d = (((weatherData.wind.deg + 360 - 22.5) / 45) % 8).toFixed(0);
+    // windDirection.textContent = directions[d];
+    // errMessage.style.display = 'none';
+  } catch (error) {
+    // errMessage.style.display = 'block';
+    console.error(error);
+  }
+};
+
+booksAlready();
+
 const defaultBooks = [
   {
     id: Math.floor(Math.random() * 1000),
